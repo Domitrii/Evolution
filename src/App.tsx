a99a82157f3a32c4bf637a5f28f9e19456c6e8e1
Domitrii <126395589+Domitrii@users.noreply.github.com>
2026-02-24 10:00:58 +0000

    The start of the project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from 'react-router-dom'
+import { lazy } from 'react';
+import './App.css'
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"))
+
+const App = () => {
+return (
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+    </Routes>
+  )
+}
+
+export default App
